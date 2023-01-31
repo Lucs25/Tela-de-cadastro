90069084cbc22597ad50055f527e138beb306fae
Lucas <lucs.25@hotmail.com>
2023-01-31 20:32:06 -0300

    adicionando validação dos input

--- js/script.js
+++ js/script.js
@@ -3,16 +3,20 @@ class Validator {
     this.validations = [
       "data-min-length",
       "data-max-length",
-    ];
+      "data-email-validate",
+      "data-required",
+      "data-only-letters",
+      "data-equal"
+    ]
   }
 
   validate(form) {
     let inputs = form.getElementsByTagName("input");
 
-    let currentValidations = document.querySelectorAll("form .error-validations")
+    let currentValidations = document.querySelectorAll("form .error-validation");
 
-    if(currentValidations.length > 0) {
-      this.cleanValidations(currentValidations)
+    if(currentValidations.length) {
+      this.cleanValidations(currentValidations);
     }
 
     let inputsArray = [...inputs];
@@ -49,17 +53,63 @@ class Validator {
     }
   }
 
-  printMessage(input, msg) {
-    let template = document.querySelector('.error-validation').cloneNode(true);
+  onlyletter() {
+    let re = /^[A-Za-z]+$/;
 
-    template.textContent = msg;
+    let inputValue = input.value;
 
-    let inputParent = input.parentNode;
+    let errorMessage = 'Este campo aceita somente letras'
 
-    template.classList.remove('template');
-
-    inputParent.appendChild(template);
+    if(!re.teste(inputValue))
+      this.printMessage(input, errorMessage);
   }
+
+  emailvalidate(input) {
+
+    let re = /\S+@\S+\.\S+/;
+
+    let email = input.value;
+
+    let errorMessage = 'Insira um e-mail padrão, ex: aaaaaa@aaaa.com';
+
+    if(!re.teste(email)) {
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  required(input) {
+    let inputValue = input.value;
+    if (inputValue ==='') {
+      let errorMessage = 'Este campo é obrigatório';
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  printMessage(input, msg) {
+
+    let errorsQty = input.parentNode.querySelector('.error-validation');
+
+    if(errorsQty === null) {
+      let template = document.querySelector('.error-validation').cloneNode(true);
+
+      template.textContent = msg;
+      let inputParent = input.parentNode;
+      template.classList.remove('template');
+      inputParent.appendChild(template);
+    }
+  }
+
+  equal(input, inputName) {
+    let inputToCompare = document.getElementsByName(inputName)[0];
+
+    let errorMessage = `Este campo precisa estar igual ao ${InputName}`;
+
+    if(input.value != inputToCompare) {
+      this.printMessage(input, errorMessage);
+    }
+  }
+
+  //verificar onde esta sendo utilizado
   cleanValidations(validations) {
     validations.forEach(el => el.remove());
   }
